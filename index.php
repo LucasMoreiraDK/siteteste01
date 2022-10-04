303eb6ae402816f81078c960abb58bd4d80152ab
LucasMoreiraDK <62969415+LucasMoreiraDK@users.noreply.github.com>
2022-10-03 23:50:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
  <head>
+  <link rel="stylesheet" href="css1.css">
   <title>Teste PHP</title>
  </head>
  <body>
  <?php echo "<p>Olá Mundo</p>"; ?>
+          <div class="divpi">
+            <center><img src="solo.jpg" width="350px" height="150px" class="imagina"></center>
+            
+                <h1>Trabalho</h1>
+                <center><p class="descr">Site de testes. 
+</p>
  </body>
 </html>
